d7b55bbab94197ab7bbf9f933ac078afdeaa5255
Gabriel Rosół <gabriell.rosoll335@gmail.com>
2022-05-10 21:06:39 +0200

    Change api approach and add date change with a button

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,94 @@
-import "package:flutter/material.dart";
-import "APOD_Data.dart";
-import "networkStuff.dart";
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "NASA ASTRONOMY PICTURE OF THE DAY",
-      theme: ThemeData(
-          primarySwatch: Colors.deepPurple,
-          scaffoldBackgroundColor: const Color.fromRGBO(118, 73, 254, 1)),
+      theme: ThemeData(scaffoldBackgroundColor: const Color(0xff37425B)),
       home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late Future<APOD_Data> futureAPODData;
+  bool _isLoading = true;
+  bool isFavourite = false;
+
+  String title = '';
+  String explanation = '';
+  String dateString = '';
+  String hdUrl = '';
+  String date = '';
+  String copyright = '';
+  String dateLink = DateFormat('yyyy-MM-dd').format(DateTime.now());
+
+  void changeDay(String dateLink) async {
+    var data = await http.get(Uri.parse(
+        'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=' +
+            dateLink));
+    var decodedData = json.decode(data.body);
+    setState(() {
+      title = decodedData['title'];
+      explanation = decodedData['explanation'];
+      dateString = decodedData['date'];
+      hdUrl = decodedData['hdurl'];
+      date = decodedData['date'];
+      copyright = decodedData['copyright'];
+    });
+  }
 
   @override
-  void initState() {
-    super.initState();
-    futureAPODData = fetchAPODData();
+  void didChangeDependencies() async {
+    if (_isLoading) {
+      print("Here");
+      print(dateLink);
+      var data = await http.get(Uri.parse(
+          'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=' +
+              dateLink));
+      var decodedData = json.decode(data.body);
+      setState(() {
+        title = decodedData['title'];
+        explanation = decodedData['explanation'];
+        dateString = decodedData['date'];
+        hdUrl = decodedData['hdurl'];
+        date = decodedData['date'];
+        copyright = decodedData['copyright'];
+        _isLoading = false;
+      });
+    }
+
+    super.didChangeDependencies();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: const Color(0xff333366),
         // ignore: prefer_const_constructors
         title: Center(
           // ignore: prefer_const_constructors
@@ -56,111 +103,127 @@ class _MyHomePageState extends State<MyHomePage> {
           IconButton(onPressed: () {}, icon: const Icon(Icons.menu))
         ],
       ),
-      body: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-        SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.width * 0.4,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
+      body: _isLoading
+          ? const CircularProgressIndicator()
+          : Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+              Expanded(
+                flex: 4,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            width: 5,
+                          ),
+                        ),
+                        child: Image.network(
+                          hdUrl,
+                          loadingBuilder: (context, child, loadingProgress) =>
+                              loadingProgress == null
+                                  ? child
+                                  //ignore: prefer_const_constructors
+                                  : Center(
+                                      child: const CircularProgressIndicator(),
+                                    ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            AutoSizeText(
+                              title,
+                              style: GoogleFonts.pacifico(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                            AutoSizeText(date,
+                                style: GoogleFonts.pacifico(fontSize: 16),
+                                textAlign: TextAlign.center),
+                            AutoSizeText(
+                              "Author: " + copyright,
+                              style: GoogleFonts.pacifico(fontSize: 16),
+                              textAlign: TextAlign.center,
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.favorite),
+                              iconSize: 40.0,
+                              color: isFavourite
+                                  ? Colors.red[400]
+                                  : Colors.grey[400],
+                              tooltip: 'Add to favorites!',
+                              onPressed: () {
+                                setState(() {
+                                  isFavourite = !isFavourite;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
               Expanded(
                 flex: 3,
-                child: FutureBuilder<APOD_Data>(
-                  future: futureAPODData,
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      Image.network(
-                        snapshot.data!.hdurl,
-                        loadingBuilder: (context, child, loadingProgress) =>
-                            loadingProgress == null
-                                ? child
-                                //ignore: prefer_const_constructors
-                                : Center(
-                                    child: const CircularProgressIndicator(),
-                                  ),
-                      );
-                    } else if (snapshot.hasError) {
-                      return Text('${snapshot.error}');
-                    }
-                    // By default, show a loading spinner.
-                    return const CircularProgressIndicator();
-                  },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 0.0, horizontal: 5.0),
+                  child: Center(
+                      child: AutoSizeText(explanation,
+                          style: const TextStyle(fontSize: 20))),
                 ),
               ),
               Expanded(
-                //flex: 1,
-                child: FutureBuilder<APOD_Data>(
-                  future: futureAPODData,
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Text(snapshot.data!.title),
-                          const SizedBox(height: 10),
-                          Text(snapshot.data!.date),
-                          const SizedBox(height: 10),
-                          Text("Author: " + snapshot.data!.copyright),
-                          const SizedBox(height: 10),
-                          IconButton(
-                            icon: const Icon(Icons.favorite),
-                            tooltip: 'Add to favorites!',
-                            onPressed: () {},
-                          ),
-                        ],
-                      );
-                    } else if (snapshot.hasError) {
-                      return Text('${snapshot.error}');
-                    }
-                    // By default, show a loading spinner.
-                    return const CircularProgressIndicator();
-                  },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.arrow_back),
+                      tooltip: 'Photo of the previous day',
+                      onPressed: () {
+                        DateTime dt = DateTime.parse(dateLink);
+                        setState(() {
+                          dateLink = DateFormat('yyyy-MM-dd')
+                                  .format(dt.subtract(const Duration(days: 1)))
+                              as String;
+                        });
+                        changeDay(dateLink);
+                        print(dateLink);
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.calendar_today_outlined),
+                      tooltip: 'Select a day from the calendar',
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.arrow_forward),
+                      tooltip: 'Photo of the next day',
+                      onPressed: () {
+                        DateTime dt = DateTime.parse(dateLink);
+                        setState(() {
+                          dateLink = DateFormat('yyyy-MM-dd')
+                                  .format(dt.add(const Duration(days: 1)))
+                              as String;
+                        });
+                        changeDay(dateLink);
+                        print(dateLink);
+                      },
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
-        SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.width * 0.2,
-          child: Center(
-            child: FutureBuilder<APOD_Data>(
-              future: futureAPODData,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  Text(snapshot.data!.explanation);
-                } else if (snapshot.hasError) {
-                  return Text('${snapshot.error}');
-                }
-                // By default, show a loading spinner.
-                return const CircularProgressIndicator();
-              },
-            ),
-          ),
-        ),
-        Expanded(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              IconButton(
-                icon: const Icon(Icons.arrow_back),
-                tooltip: 'Photo of the previous day',
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: const Icon(Icons.calendar_today_outlined),
-                tooltip: 'Select a day from the calendar',
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: const Icon(Icons.arrow_forward),
-                tooltip: 'Photo of the next day',
-                onPressed: () {},
-              ),
-            ],
-          ),
-        ),
-      ]),
+            ]),
     );
   }
 }
--- lib/networkStuff.dart
+++ lib/networkStuff.dart
@@ -6,17 +6,19 @@ import 'dart:async';
 import 'dart:convert';
 
 Future<APOD_Data> fetchAPODData() async {
-  const url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+  const url =
+      "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2022-04-05";
 
   final response = await http.get(Uri.parse(url));
 
-  if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
-    return APOD_Data.fromJson(jsonDecode(response.body));
-  } else {
+  // if (response.statusCode == 200) {
+  // If the server did return a 200 OK response,
+  // then parse the JSON.
+  return APOD_Data.fromJson(jsonDecode(response.body));
+}
+  // else {
     // If the server did not return a 200 OK response,
     // then throw an exception.
-    throw Exception('Failed to load APOD_Data');
-  }
-}
+//     throw Exception('Failed to load APOD_Data');
+//   }
+// }
